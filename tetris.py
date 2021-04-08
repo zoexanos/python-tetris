8d3434ccbc48d063deac3f0397662ade0db94414
ToveXan <zxanoswork@gmail.com>
2021-04-07 20:05:54 -0500

    filling in the base functions

--- tetris.py
+++ tetris.py
@@ -1,4 +1,164 @@
 import pygame
 import random
 
+"""
+10 x 20 square grid
+shapes: S, Z, I, O, J, L, T
+represented in order by 0 - 6
+"""
 
+pygame.font.init()
+
+#GLOBAL VARIABLES
+s_width = 800
+s_height = 700
+play_width = 300
+play_height = 600
+block_size = 30
+
+top_left_x = (s_width - play_width) // 2
+top_left_y = s_height - play_height
+
+S = [['.....',
+      '.....',
+      '..00.',
+      '.00..',
+      '.....'],
+      ['.....',
+      '..0..',
+      '..00.',
+      '...0.',
+      '.....']]
+
+Z = [['.....',
+      '.....',
+      '.00..',
+      '..00.',
+      '.....'],
+      ['.....',
+      '..0..',
+      '.00..',
+      '.0...',
+      '.....']]
+
+I = [['..0..',
+      '..0..',
+      '..0..',
+      '..0..',
+      '.....'],
+      ['.....',
+      '.00..',
+      '.00..',
+      '.....',
+      '.....']]
+
+O = [['.....',
+      '.00..',
+      '.00..',
+      '.....',
+      '.....']]
+
+J = [['.....',
+      '.0...',
+      '.000.',
+      '.....',
+      '.....'],
+      ['.....',
+      '..00.',
+      '..0..',
+      '..0..',
+      '.....'],
+      ['.....',
+      '.....',
+      '.000.',
+      '...0.',
+      '.....'],
+      ['.....',
+      '..0..',
+      '..0..',
+      '.00..',
+      '.....']]
+
+L = [['.....',
+      '...0.',
+      '.000.',
+      '.....',
+      '.....'],
+      ['.....',
+      '..0..',
+      '..0..',
+      '..00.',
+      '.....'],
+      ['.....',
+      '.....',
+      '.000.',
+      '.0...',
+      '.....'],
+      ['.....',
+      '.00..',
+      '..0..',
+      '..0..',
+      '.....']]
+
+T = [['.....',
+      '..0..',
+      '.000.',
+      '.....',
+      '.....'],
+      ['.....',
+      '..0..',
+      '..00.',
+      '..0..',
+      '.....'],
+      ['.....',
+      '.....',
+      '.000.',
+      '..0..',
+      '.....'],
+      ['.....',
+      '..0..',
+      '.00..',
+      '..0..',
+      '.....']]
+
+shapes = [S, Z, I, O, J, L, T]
+shape_colors = [(0,255,0), (255,0,0), (0,255,255), (255,255,0), (255,165,0), (0,0,255), (128,0,128)]
+#index 0 - 6 represent shape
+
+class Piece(object):
+  pass
+
+def create_grid(locked_positions = {}):
+  pass
+
+def convert_shape_format(shape):
+  pass
+
+def valid_space(shape, grid):
+  pass
+
+def check_lost(positions):
+  pass
+
+def get_shape():
+  pass
+
+def draw_text_middle(text, size, color, surface):
+  pass
+
+def clear_rows(grid, locked):
+  pass
+
+def draw_next_shape(shape, surface):
+  pass
+
+def draw_window(surface):
+  pass
+
+def main():
+  pass
+
+def main_menu():
+  pass
+
+main_menu()
